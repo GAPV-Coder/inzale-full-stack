e55c89afb50e60b4f72fc122949c63be3f84347c
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2025-06-13 22:28:02 -0500

    feat: Create Service and Controller for authentication [Auth-Service]

---
+++ backend-app/auth-service/src/modules/auth.controller.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './auth/dto/register.dto';
+import { LoginDto } from './auth/dto/login.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('register')
+    @ApiOperation({ summary: 'Register a new user' })
+    @ApiResponse({ status: 201, description: 'User registered successfully' })
+    async register(@Body() registerDto: RegisterDto) {
+        return this.authService.register(registerDto);
+    }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Login a user' })
+    @ApiResponse({ status: 200, description: 'User logged in successfully' })
+    async login(@Body() loginDto: LoginDto) {
+        return this.authService.login(loginDto);
+    }
+}
---
+++ backend-app/auth-service/src/modules/auth.service.ts
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from './auth/entities/user.entity';
+import { RegisterDto } from './auth/dto/register.dto';
+import { LoginDto } from './auth/dto/login.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectModel(User.name) private userModel: Model<User>,
+        private jwtService: JwtService,
+    ) { }
+
+    async register(registerDto: RegisterDto): Promise<{ token: string }> {
+        const { email, password, role } = registerDto;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new this.userModel({ email, password: hashedPassword, role });
+        await user.save();
+        const payload = { sub: user._id, email: user.email, role: user.role };
+        return { token: this.jwtService.sign(payload) };
+    }
+
+    async login(loginDto: LoginDto): Promise<{ token: string }> {
+        const { email, password } = loginDto;
+        const user = await this.userModel.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        const payload = { sub: user._id, email: user.email, role: user.role };
+        return { token: this.jwtService.sign(payload) };
+    }
+}
